6e7ac519b526644f667ba00b7f9ea133537c214e
PepeLuis <jpingoy@hotmail.com>
2026-02-27 13:37:31 -0600

    mensaje cambiado

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hola pingoy!");
+  Serial.println("Hola pingoy se cambio el mensaje!");
   delay(1000);
 }
